7f6c8975f5205be7ede9e7e7c00346cd2fe4224e
Niccatania <Niccatania6@gmail.com>
2023-01-19 00:47:35 -0600

    add useState to handle search capturing

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,30 +1,38 @@
+import React, {useState} from 'react'
+
 export default function Landing() {
+  const [searchInput, setSearchInput ] = useState("");
+
+  function handleSearch(event){
+  const userInput = event.target.value;
+  setSearchInput(userInput)
+  }
+  function search(event) {
+    event.preventDefault();
+    console.log(searchInput);
+  }
+
+
     return (
       <>
         <div id="sidebar">
           <h1>Strainabase</h1>
           <div>
-            <form id="search-form" role="search">
+            <form id="search-form" role="search"
+            onSubmit={search}>
               <input
-                id="q"
+                id="input"
                 aria-label="Search strain"
                 placeholder="Search"
                 type="search"
-                name="q"
+                name="input"
+                onChange={handleSearch}
               />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
+                 <button type="submit">Search</button>
             </form>
-            <form method="post">
-              <button type="submit">Search</button>
-            </form>
+           
+           
+          
           </div>
           <nav>
            
